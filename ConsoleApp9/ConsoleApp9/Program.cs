5fda6b1e89b148a5d3b8f5c1baf74f27cb891da6
O1Wilson <Badgermouse583@gmail.com>
2024-02-18 03:14:20 -0600

    new

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class MethodOverloading
+{
+    public int Overloading(int num)
+    {
+        int result = num + 5;
+        return result;
+    }
+
+    public int Overloading(decimal num)
+    {
+        int result = Convert.ToInt32(num * 2);
+        return result;
+    }
+
+    public int Overloading(string numString)
+    {
+        if (int.TryParse(numString, out int num))
+        {
+            int result = num * num;
+            return result;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please provide a valid integer.");
+            return 0;
+        }
+    }
+
+    static void Main()
+    {
+        MethodOverloading mathOps = new MethodOverloading();
+
+        int result1 = mathOps.Overloading(10);
+        Console.WriteLine($"Result of integer operation: {result1}");
+
+        int result2 = mathOps.Overloading(7.5m);
+        Console.WriteLine($"Result of decimal operation: {result2}");
+
+        int result3 = mathOps.Overloading("15");
+        Console.WriteLine($"Result of string-to-integer operation: {result3}");
+
+        int result4 = mathOps.Overloading("InvalidInput");
+        Console.ReadLine();
+    }
+}
