3037aa8be36d758a2ca2531fa06e1e7b30ce6cac
O1Wilson <Badgermouse583@gmail.com>
2024-02-19 00:05:12 -0600

    new

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter the current day of the week: ");
+            string userInput = Console.ReadLine();
+
+            DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, true);
+
+            Console.WriteLine($"You entered: {currentDay}");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Please enter an actual day of the week.");
+        }
+    }
+}
