506d3d5af375d715a4f04ff4cff8cfd836d15872
O1Wilson <Badgermouse583@gmail.com>
2024-02-19 14:12:50 -0600

    new

--- ConsoleApp8/ConsoleApp8/Program.cs
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -6,26 +6,29 @@ class MathOperations
     {
         int inputNumber = 5;
 
-        int result1 = Square(inputNumber);
-        int result2 = Double(inputNumber);
-        int result3 = AddTen(inputNumber);
+        int result1 = Program.Square(inputNumber);
+        int result2 = Program.Double(inputNumber);
+        int result3 = Program.AddTwenty(inputNumber);
 
         Console.WriteLine($"Square: {result1}");
         Console.WriteLine($"Quintuple: {result2}");
         Console.WriteLine($"Twenty: {result3}");
     }
+}
 
-    static int Square(int number)
+class Program
+{
+    public static int Square(int number)
     {
         return number * number;
     }
 
-    static int Double(int number)
+    public static int Double(int number)
     {
         return number * 5;
     }
 
-    static int AddTen(int number)
+    public static int AddTwenty(int number)
     {
         return number + 20;
     }
