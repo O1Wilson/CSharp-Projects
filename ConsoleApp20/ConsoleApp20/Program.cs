af7b0855dab7c9c9413e9873b96d014dd632967e
O1Wilson <Badgermouse583@gmail.com>
2024-02-19 00:50:53 -0600

    new

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        DateTime currentDateTime = DateTime.Now;
+        Console.WriteLine($"Current Date and Time: {currentDateTime}");
+
+        Console.Write("Enter a number: ");
+        string userInput = Console.ReadLine();
+
+        if (int.TryParse(userInput, out int hoursToAdd))
+        {
+            DateTime futureDateTime = currentDateTime.AddHours(hoursToAdd);
+            Console.WriteLine($"The time in {hoursToAdd} hours will be: {futureDateTime}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+
+        Console.ReadLine();
+    }
+}
