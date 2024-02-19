58016b6fdf57364ba463c479b34e0d3fa77ebc50
O1Wilson <Badgermouse583@gmail.com>
2024-02-18 23:38:30 -0600

    new

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Employee<T>
+{
+    public List<T> Things { get; set; }
+
+    public Employee(List<T> things)
+    {
+        Things = things;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee<string> stringEmployee = new Employee<string>(new List<string> { "Apple", "Banana", "Cherry" });
+        Employee<int> intEmployee = new Employee<int>(new List<int> { 1, 2, 3, 4, 5 });
+
+        Console.WriteLine("String Employee Things:");
+        foreach (var thing in stringEmployee.Things)
+        {
+            Console.WriteLine(thing);
+        }
+
+        Console.WriteLine("\nInt Employee Things:");
+        foreach (var thing in intEmployee.Things)
+        {
+            Console.WriteLine(thing);
+        }
+    }
+}
