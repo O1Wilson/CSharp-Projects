25f70942eef49a76887068e3a92df470854b4c54
O1Wilson <Badgermouse583@gmail.com>
2024-02-18 20:32:49 -0600

    new

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public abstract class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public abstract void SayName();
+}
+
+public class Employee : Person
+{
+    public override void SayName()
+    {
+        Console.WriteLine($"Employee's name is {FirstName} {LastName}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee employee = new Employee
+        {
+            FirstName = "George",
+            LastName = "Brian"
+        };
+
+        employee.SayName();
+    }
+}
