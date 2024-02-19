2f444727908b0ddacb678cb3f543bb8a3491e2bd
O1Wilson <Badgermouse583@gmail.com>
2024-02-19 14:06:29 -0600

    new

--- ConsoleApp12/ConsoleApp12/Program.cs
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 
-public class ClassAssignment
+public static class ClassAssignment
 {
-    public void DivideByTwo(int input)
+    public static void DivideByTwo(int input)
     {
         int result = input / 2;
         Console.WriteLine($"Dividing {input} by 2: {result}");
     }
 
-    public void MultiplyAndDivide(int x, int y, out int product, out int quotient)
+    public static void MultiplyAndDivide(int x, int y, out int product, out int quotient)
     {
         product = x * y;
         quotient = x / y;
     }
 
-    public void MultiplyAndDivide(int x, int y)
+    public static void MultiplyAndDivide(int x, int y)
     {
         int product = x * y;
         int quotient = x / y;
@@ -26,16 +26,14 @@ class Program
 {
     static void Main()
     {
-        ClassAssignment classAssignment = new ClassAssignment();
-
         Console.Write("Enter a number: ");
         int userInput = Convert.ToInt32(Console.ReadLine());
 
-        classAssignment.DivideByTwo(userInput);
+        ClassAssignment.DivideByTwo(userInput);
 
-        classAssignment.MultiplyAndDivide(userInput, 3, out int product, out int quotient);
+        ClassAssignment.MultiplyAndDivide(userInput, 3, out int product, out int quotient);
         Console.WriteLine($"Product: {product}, Quotient: {quotient}");
 
-        classAssignment.MultiplyAndDivide(userInput, 2);
+        ClassAssignment.MultiplyAndDivide(userInput, 2);
     }
 }
