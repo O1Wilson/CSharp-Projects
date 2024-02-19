9f4e77fb84a483eb8084e860d938625bf1685a0e
O1Wilson <Badgermouse583@gmail.com>
2024-02-18 19:09:00 -0600

    new

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public void SayName()
+    {
+        Console.WriteLine($"Name: {FirstName} {LastName}");
+    }
+}
+
+class Employee : Person
+{
+    public int Id { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee employee = new Employee
+        {
+            FirstName = "Kevin",
+            LastName = "Moser",
+            Id = 123 
+        };
+
+        employee.SayName();
+
+        Console.WriteLine($"Employee Id: {employee.Id}");
+    }
+}
